153017c85c619001fc03346eab076e95d12d15a8
ShKhannanov <skhannanov@spotware.com>
2023-04-23 12:29:15 +0300

    Task F4 added, added QuestionWidget

--- flutter-basics/question/lib/main.dart
+++ flutter-basics/question/lib/main.dart
@@ -9,11 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Question questionOne =
-        Question('Sciences', 'How many fish are there in the world?');
-    final Question questionTwo = Question('Arts', 'What is Da Vinci\'s name?');
-    final Question questionThree =
-        Question('History', 'When was Pump Up the Jam (the song) released?');
     return MaterialApp(
         title: 'Quiz App',
         theme: ThemeData(
@@ -26,168 +21,144 @@ class MyApp extends StatelessWidget {
             title: const Text('Quiz App'),
           ),
           body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                              height: 70,
-                              width: 70,
-                              child: Image.asset('assets/${questionOne.image}'))
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                        flex: 3,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text('Question 1',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text(questionOne.category),
-                            Text(questionOne.text)
-                          ],
-                        )),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                              height: 70,
-                              width: 70,
-                              child: Image.asset('assets/${questionTwo.image}'))
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                        flex: 3,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text('Question 2',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text(questionTwo.category),
-                            Text(questionTwo.text)
-                          ],
-                        )),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                              height: 70,
-                              width: 70,
-                              child:
-                                  Image.asset('assets/${questionThree.image}'))
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                        flex: 3,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            const Text('Question 3',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text(questionThree.category),
-                            Text(
-                              questionThree.text,
-                              textAlign: TextAlign.center,
-                            )
-                          ],
-                        )),
-                  ],
-                ),
-              ],
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const QuestionWidget(
+                    questionText: 'How many fish are there in the world?',
+                    questionCategory: 'Sciences',
+                  ),
+                  const QuestionWidget(
+                    questionText: 'What is Da Vinci\'s name?',
+                    questionCategory: 'Arts',
+                  ),
+                  const QuestionWidget(
+                    questionText:
+                        'When was Pump Up the Jam (the song) released?',
+                    questionCategory: 'History',
+                  ),
+                  const QuestionWidget(
+                    questionText:
+                        'What is the name of the smallest unit of life?',
+                    questionCategory: 'Sciences',
+                  ),
+                  const QuestionWidget(
+                    questionText:
+                        'In physics, what term is used to describe the resistance of an object to changes in its state of motion?',
+                    questionCategory: 'Sciences',
+                  ),
+                  const QuestionWidget(
+                    questionText:
+                        'What is the name of the character played by Hugh Jackman in the X-Men movies?',
+                    questionCategory: 'Arts',
+                  ),
+                  const QuestionWidget(
+                    questionText:
+                        'Who wrote the novel "The Catcher in the Rye"?',
+                    questionCategory: 'Arts',
+                  ),
+                  const QuestionWidget(
+                    questionText:
+                        'What is the highest-grossing film of all time (not adjusted for inflation)?',
+                    questionCategory: 'Arts',
+                  ),
+                  const QuestionWidget(
+                    questionText:
+                        'Who was the first president of the United States of America?',
+                    questionCategory: 'History',
+                  ),
+                  const QuestionWidget(
+                    questionText: 'In what year did World War II end?',
+                    questionCategory: 'History',
+                  ),
+                  const QuestionWidget(
+                    questionText:
+                        'Who was the first female Prime Minister of the United Kingdom?',
+                    questionCategory: 'History',
+                  ),
+                  const QuestionWidget(
+                    questionText: 'In which sport would you use a shuttlecock?',
+                    questionCategory: 'Badminton',
+                  ),
+                ],
+              ),
             ),
           ),
         ));
   }
 }
 
-class Question {
-  late String _category;
-  late String _text;
-  late String _image;
+class QuestionWidget extends StatelessWidget {
+  const QuestionWidget({
+    super.key,
+    required this.questionText,
+    required this.questionCategory,
+  });
 
-  String get category => _category;
-  set category(String category) {
-    try {
-      if (category.length > 0) {
-        _category = category;
-      }
-    } catch (e) {
-      rethrow;
-    }
+  final String questionText;
+  final String questionCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                  height: 70,
+                  width: 70,
+                  child: Image.asset('assets/${createImage(questionCategory)}'))
+            ],
+          ),
+        ),
+        Expanded(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text('Question!',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                Text(
+                  questionCategory,
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  questionText,
+                  textAlign: TextAlign.center,
+                )
+              ],
+            )),
+        const SizedBox(
+          height: 100,
+        )
+      ],
+    );
   }
+}
 
-  String get text => _text;
-  set text(String text) {
-    try {
-      if (text.length > 0) {
-        _text = text;
-      }
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  String get image => _image;
-  set image(String category) {
-    try {
-      if (category.length > 0) {
-        _image = createImage(category);
-      }
-    } catch (e) {
-      rethrow;
-    }
-  }
-
-  Question(String category, String text) {
-    this.category = category;
-    this.text = text;
-    this.image = category;
-  }
-
-  String createImage(String category) {
-    switch (category) {
-      case 'History':
-        return 'history.png';
-      case 'Geography':
-        return 'geography.png';
-      case 'Sciences':
-        return 'sciences.png';
-      case 'Sports':
-        return 'sports.png';
-      case 'Arts':
-        return 'arts.png';
-      case 'Entertainment':
-        return 'entertainment.png';
-      default:
-        return 'sciences.png';
-    }
+String createImage(String category) {
+  switch (category) {
+    case 'History':
+      return 'history.png';
+    case 'Geography':
+      return 'geography.png';
+    case 'Sciences':
+      return 'sciences.png';
+    case 'Sports':
+      return 'sports.png';
+    case 'Arts':
+      return 'arts.png';
+    case 'Entertainment':
+      return 'entertainment.png';
+    default:
+      return 'sciences.png';
   }
 }
