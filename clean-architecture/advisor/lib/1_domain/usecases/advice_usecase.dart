3ab77aa59a66e5c1c0679b0574fbf87af1efa265
ShKhannanov <skhannanov@spotware.com>
2023-06-04 10:58:25 +0300

    Use cases added

--- clean-architecture/advisor/lib/1_domain/usecases/advice_usecase.dart
+++ clean-architecture/advisor/lib/1_domain/usecases/advice_usecase.dart
@@ -1,11 +1,16 @@
 import 'package:advisor/1_domain/entities/advice_entity.dart';
+import 'package:advisor/1_domain/failures/failures.dart';
+import 'package:dartz/dartz.dart';
 
 class AdviceUseCases {
-  Future<AdviceEntity> getAdvice() async {
+  Future<Either<Failure, AdviceEntity>> getAdvice() async {
     await Future.delayed(const Duration(seconds: 3), () {});
-    return AdviceEntity(
-      advice: 'fake advice',
-      adviceId: 28,
+    return right(
+      AdviceEntity(
+        advice: 'fake advice',
+        adviceId: 28,
+      ),
     );
+    return left(ServerFailure());
   }
 }
