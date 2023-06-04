19e6a7716c5dd2532a0bee4fb910385f7e630ab3
ShKhannanov <skhannanov@spotware.com>
2023-06-04 11:20:24 +0300

    Repo modification

---
+++ clean-architecture/advisor/lib/0_data/repositories/advice_repo_impl.dart
@@ -0,0 +1,10 @@
+import 'package:advisor/1_domain/entities/advice_entity.dart';
+import 'package:advisor/1_domain/repositories/advice_repo.dart';
+
+class AdviceRepoImpl implements AdviceRepo {
+  @override
+  Future<AdviceEntity> getAdviceFromDataSource() {
+    // TODO: implement getAdviceFromDataSource
+    throw UnimplementedError();
+  }
+}
