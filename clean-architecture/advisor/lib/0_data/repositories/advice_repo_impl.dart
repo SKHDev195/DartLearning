16662fed48a378dd078c5377db32d404aa5b568a
ShKhannanov <skhannanov@spotware.com>
2023-06-04 15:51:33 +0300

    Basic error handling added

--- clean-architecture/advisor/lib/0_data/repositories/advice_repo_impl.dart
+++ clean-architecture/advisor/lib/0_data/repositories/advice_repo_impl.dart
@@ -1,10 +1,25 @@
+import 'package:advisor/0_data/datasources/advice_remote_datasource.dart';
+import 'package:advisor/0_data/exceptions/exceptions.dart';
 import 'package:advisor/1_domain/entities/advice_entity.dart';
+import 'package:advisor/1_domain/failures/failures.dart';
 import 'package:advisor/1_domain/repositories/advice_repo.dart';
+import 'package:dartz/dartz.dart';
 
 class AdviceRepoImpl implements AdviceRepo {
+  final AdviceRemoteDatasource adviceRemoteDataSource =
+      AdviceRemoteDatasourceImpl();
+
   @override
-  Future<AdviceEntity> getAdviceFromDataSource() {
-    // TODO: implement getAdviceFromDataSource
-    throw UnimplementedError();
+  Future<Either<Failure, AdviceEntity>> getAdviceFromDataSource() async {
+    try {
+      final result = await adviceRemoteDataSource.getRandomAdviceFromApi();
+      return right(result);
+    } on ServerException catch (e) {
+      return left(ServerFailure());
+    } on CacheException catch (e) {
+      return left(CacheFailure());
+    } catch (e) {
+      return left(GeneralFailure());
+    }
   }
 }
