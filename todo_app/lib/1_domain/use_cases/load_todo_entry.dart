02ae01d3dd9c989c0612a768fd69f57204ebe441
ShKhannanov <skhannanov@spotware.com>
2023-06-21 20:24:50 +0300

    Entry loading added

---
+++ todo_app/lib/1_domain/use_cases/load_todo_entry.dart
@@ -0,0 +1,35 @@
+import 'package:either_dart/either.dart';
+
+import 'package:todo_app/1_domain/failures/failure.dart';
+import 'package:todo_app/1_domain/repositories/todo_repository.dart';
+
+import '../../core/use_case.dart';
+import '../entities/todo_entry.dart';
+
+class LoadToDoEntry implements UseCase<ToDoEntry, ToDoEntryIdsParams> {
+  const LoadToDoEntry({
+    required this.toDoRepository,
+  });
+
+  final ToDoRepository toDoRepository;
+
+  @override
+  Future<Either<Failure, ToDoEntry>> call(ToDoEntryIdsParams params) async {
+    try {
+      final loadedEntry = toDoRepository.readToDoEntry(
+        params.collectionId,
+        params.entryId,
+      );
+      return loadedEntry.fold(
+        (left) => Left(left),
+        (right) => Right(right),
+      );
+    } on Exception catch (e) {
+      return Left(
+        ServerFailure(
+          stackTrace: e.toString(),
+        ),
+      );
+    }
+  }
+}
--- todo_app/lib/core/use_case.dart
+++ todo_app/lib/core/use_case.dart
@@ -1,5 +1,6 @@
 import 'package:either_dart/either.dart';
 import 'package:equatable/equatable.dart';
+import 'package:todo_app/1_domain/entities/unique_id.dart';
 import 'package:todo_app/1_domain/failures/failure.dart';
 
 abstract class UseCase<Type, Params> {
@@ -11,4 +12,33 @@ abstract class Params extends Equatable {}
 class NoParams extends Params {
   @override
   List<Object?> get props => [];
+}
+
+class ToDoEntryIdsParams extends Params {
+  ToDoEntryIdsParams({
+    required this.collectionId,
+    required this.entryId,
+  }) : super();
+
+  final EntryId entryId;
+  final CollectionId collectionId;
+
+  @override
+  List<Object?> get props => [
+        entryId,
+        collectionId,
+      ];
+}
+
+class CollectionIdParams extends Params {
+  CollectionIdParams({
+    required this.collectionId,
+  }) : super();
+
+  final CollectionId collectionId;
+
+  @override
+  List<Object?> get props => [
+        collectionId,
+      ];
 }
