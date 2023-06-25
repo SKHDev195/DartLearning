81cf2b5aa1afd68e5cb2e1a4da2d0eb370346de5
ShKhannanov <skhannanov@spotware.com>
2023-06-25 13:08:04 +0300

    Collection details added

--- todo_app/lib/2_application/core/routes.dart
+++ todo_app/lib/2_application/core/routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:todo_app/1_domain/entities/unique_id.dart';
 import 'package:todo_app/2_application/core/go_router_observer.dart';
 import 'package:todo_app/2_application/pages/dashboard/dashboard_page.dart';
+import 'package:todo_app/2_application/pages/detail/todo_detail_page.dart';
 import 'package:todo_app/2_application/pages/home/home_page.dart';
+import 'package:todo_app/2_application/pages/overview/overview_page.dart';
 import 'package:todo_app/2_application/pages/settings/settings_page.dart';
-import 'package:todo_app/2_application/pages/task/task_page.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(
   debugLabel: 'root',
@@ -29,7 +31,34 @@ final routes = GoRouter(
         return const SettingsPage();
       },
     ),
-    GoRoute(path: path)
+    GoRoute(
+      name: ToDoDetailPage.pageConfig.name,
+      path: '$_basePath/overview/:collectionId',
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('details'),
+            leading: BackButton(
+              onPressed: () {
+                if (context.canPop()) {
+                  context.pop();
+                } else {
+                  context.pushNamed(
+                    HomePage.pageConfig.name,
+                    pathParameters: {'tab': OverviewPage.pageConfig.name},
+                  );
+                }
+              },
+            ),
+          ),
+          body: ToDoDetailPageProvider(
+            collectionId: CollectionId.fromUniqueString(
+              state.pathParameters['collectionId'] ?? '',
+            ),
+          ),
+        );
+      },
+    ),
     ShellRoute(
       navigatorKey: _shellNavigatorKey,
       builder: (context, state, child) => child,
---
+++ todo_app/lib/2_application/pages/detail/bloc/cubit/to_do_detail_cubit.dart
@@ -0,0 +1,43 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+import 'package:todo_app/1_domain/entities/unique_id.dart';
+import 'package:todo_app/1_domain/use_cases/load_todo_entry_ids_for_collection.dart';
+import 'package:todo_app/core/use_case.dart';
+
+part 'to_do_detail_cubit_state.dart';
+
+class ToDoDetailCubit extends Cubit<ToDoDetailCubitState> {
+  ToDoDetailCubit({
+    required this.collectionId,
+    required this.loadToDoEntryIdsForCollection,
+  }) : super(const ToDoDetailCubitLoadingState());
+
+  final CollectionId collectionId;
+  final LoadToDoEntryIdsForCollection loadToDoEntryIdsForCollection;
+
+  Future<void> fetch() async {
+    emit(ToDoDetailCubitLoadingState());
+
+    try {
+      final entryIds = await loadToDoEntryIdsForCollection.call(
+        CollectionIdParams(
+          collectionId: collectionId,
+        ),
+      );
+
+      if (entryIds.isLeft) {
+        emit(const ToDoDetailCubitErrorState());
+      } else {
+        emit(
+          ToDoDetailCubitLoadedState(
+            entryIds: entryIds.right,
+          ),
+        );
+      }
+    } on Exception {
+      emit(const ToDoDetailCubitErrorState());
+    }
+  }
+}
