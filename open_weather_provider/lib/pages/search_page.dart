96ecdecae9f80916aef1323ef178479b65453fd9
ShKhannanov <skhannanov@spotware.com>
2023-08-06 16:01:14 +0300

    Weather app completed

--- open_weather_provider/lib/pages/search_page.dart
+++ open_weather_provider/lib/pages/search_page.dart
@@ -8,6 +8,28 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final _formKey = GlobalKey<FormState>();
+  String? _city;
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  void _submit() {
+    setState(
+      () {
+        autovalidateMode = AutovalidateMode.always;
+      },
+    );
+
+    final form = _formKey.currentState;
+
+    if (form != null && form.validate()) {
+      form.save();
+      Navigator.pop(
+        context,
+        _city!.trim(),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +38,52 @@ class _SearchPageState extends State<SearchPage> {
           child: Text('Search'),
         ),
       ),
-      body: const Center(
-        child: Text('Search'),
+      body: Form(
+        key: _formKey,
+        autovalidateMode: autovalidateMode,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 60,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextFormField(
+                autofocus: true,
+                style: const TextStyle(
+                  fontSize: 18,
+                ),
+                decoration: const InputDecoration(
+                  labelText: 'City name',
+                  hintText: 'Has to be more than 2 characters',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? input) {
+                  if (input == null || input.trim().length < 2) {
+                    return 'City name must be at least 2 characters long';
+                  }
+                  return null;
+                },
+                onSaved: (String? input) {
+                  _city = input;
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: _submit,
+              child: const Text(
+                'Check the weather',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- open_weather_provider/lib/pages/settings_page.dart
+++ open_weather_provider/lib/pages/settings_page.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
+import '../providers/providers.dart';
 
-  @override
-  State<SettingsPage> createState() => _SettingsPageState();
-}
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key? key}) : super(key: key);
 
-class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(
-          child: Text('Settings'),
-        ),
+        title: const Text('Settings'),
       ),
-      body: const Center(
-        child: Text('Settings'),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListTile(
+          title: const Text('Temperature Unit'),
+          subtitle: const Text('Celsius/Fahrenheit (Default: Celsius)'),
+          trailing: Switch(
+            value: context.watch<TempSettingsProvider>().state.tempUnit ==
+                TempUnit.celsius,
+            onChanged: (_) {
+              context.read<TempSettingsProvider>().toggleTempUnit();
+            },
+          ),
+        ),
       ),
     );
   }
